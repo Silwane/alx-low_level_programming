e7ebbde42228db5bd8b926c211268d641448b483
Silwane <ephraim.mdluli@gmail.com>
2022-07-03 12:02:49 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- Prints out a string to stdout.
+ * Return: 0 if successful.
+ */
+int main(void);
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (0);
+}

f03bdb4a9fa5a3342857406afef37d22f8c0eaa9
Silwane <ephraim.mdluli@gmail.com>
2022-07-04 11:38:51 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,19 +5,15 @@
  *
  * Retun: Always (Successful)
  */
-int main()
+int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-	}
 
 	for (c = 'A'; c <= 'Z'; c++)
-	{
 		putchar(c);
-	}
 
 	putchar('\n');
 

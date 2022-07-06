d4e255e8bd4f9fbedde9e3b62ba45860d479dda7
Silwane <ephraim.mdluli@gmail.com>
2022-07-06 07:54:50 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('M');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}

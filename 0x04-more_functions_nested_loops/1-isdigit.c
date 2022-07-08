1e056360b85ae27bd788b8c2655c3d2f147e7342
Silwane <ephraim.mdluli@gmail.com>
2022-07-08 15:51:23 -0700

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * mul - Multiplies two integers
- * @x: The first number to multiply
- * @y: The second number to multiply with first number
- * Return: The result of multiplication between x param and y param
+ * _isdigit - Check if a character is a digit
+ * @x: The number to be checked
+ *
+ *Return: 1 for a character that will be a digit or 0 for any else
  */
 
-int mul(int x, int y)
+int _isdigit(int x)
 {
-	return (x * y);
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+	
+	return (0);
 }
